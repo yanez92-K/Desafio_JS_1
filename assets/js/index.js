7f0efede0babdcbffa416e8e7223a3323918a704
Yanez92-k <Yanez.Peredo.P.K@outlook.com>
2024-08-21 18:50:50 -0400

    resultado de multiplicacion

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,28 @@
-precio = 400000
+precio = 400000;
 
 precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio 
-cantidad = document.querySelector(".cantidad")
+precioSpan.innerHTML = precio;
+cantidad = document.querySelector(".cantidad");
 
-function incrementar(){
-    parrafo = document.querySelector("p .cantidad")
-    cuenta = Number (parrafo.innerHTML)
-    cuenta = cuenta + 1
-    parrafo.innerHTML = cuenta
+function incrementar() {
+  parrafo = document.querySelector("p .cantidad");
+  cuenta = Number(parrafo.innerHTML);
+  cuenta = cuenta + 1;
+  parrafo.innerHTML = cuenta;
 }
 function decrementar() {
-    parrafo = document.querySelector("p .cantidad");
-    cuenta = Number(parrafo.innerHTML);
-    cuenta = cuenta - 1;
-    parrafo.innerHTML = cuenta;
+  parrafo = document.querySelector("p .cantidad");
+  cuenta = Number(parrafo.innerHTML);
+  cuenta = cuenta - 1;
+  parrafo.innerHTML = cuenta;
 }
 
-
+const button = document.querySelector(".button");
+button.addEventListener("click", () => {
+  const precioSpan = document.querySelector(".precio-inicial").value;
+  console.log("precioSpan-->>", precioSpan);
+  const cantidad = document.querySelector(".cantidad").value;
+  console.log("cantidad-->>", cantidad);
+  const total = cuenta * precio;
+  document.querySelector(".valor-total").innerHTML = total;
+});

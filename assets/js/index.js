daa7d6855c65624a1adf49705a7713d93c7e897e
Yanez92-k <Yanez.Peredo.P.K@outlook.com>
2024-08-22 17:27:06 -0400

    actualizacion final

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,21 @@
-precio = 400000;
-
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio;
-cantidad = document.querySelector(".cantidad");
+let cantidad = 0;
+let precioBase = 400000;
+let precioTotal = 0;
 
 function incrementar() {
-  parrafo = document.querySelector("p .cantidad");
-  cuenta = Number(parrafo.innerHTML);
-  cuenta = cuenta + 1;
-  parrafo.innerHTML = cuenta;
-}
-function decrementar() {
-  parrafo = document.querySelector("p .cantidad");
-  cuenta = Number(parrafo.innerHTML);
-  cuenta = cuenta - 1;
-  parrafo.innerHTML = cuenta;
+  cantidad++;
+  actualizarCantidadYTotal();
 }
 
-const button = document.querySelector(".button");
-button.addEventListener("click", () => {
-  const precioSpan = document.querySelector(".precio-inicial").value;
-  console.log("precioSpan-->>", precioSpan);
-  const cantidad = document.querySelector(".cantidad").value;
-  console.log("cantidad-->>", cantidad);
-  const total = cuenta * precio;
-  document.querySelector(".valor-total").innerHTML = total;
-});
+function decrementar() {
+  if (cantidad > 0) {
+    cantidad--;
+    actualizarCantidadYTotal();
+  }
+}
+
+function actualizarCantidadYTotal() {
+  document.querySelector(".cantidad").innerHTML = cantidad;
+  precioTotal = cantidad * precioBase;
+  document.querySelector(".valor-total").innerHTML = precioTotal;
+}
